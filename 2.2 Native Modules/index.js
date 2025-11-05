ab549e34a11e86eeb3bda80a6a63dcfbaefb3348
Divyadharshini <divyadharshini.mb.13@gmail.com>
2025-11-05 20:32:33 +0530

    uploading new codes

---
+++ 2.2 Native Modules/index.js
@@ -0,0 +1,10 @@
+const fs=require("fs");
+fs.writeFile("message.txt","Hello from Node.js",(err)=>{
+    if(err) throw err;
+    console.log("This file has been saved");
+});
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
